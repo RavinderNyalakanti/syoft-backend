2d9e8ab3c9ae750caf3e0eea3ea2edf427b4f1cd
RavinderNyalakanti <ravindernyalakanti8@gmail.com>
2024-07-15 17:10:15 +0530

    skfjfc

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-require('dotenv').config(); 
 const express = require('express');
-const { sequelize } = require('./config/db');
+const dotenv = require('dotenv');
+const { connectDB } = require('./config/db');
+const { syncModels } = require('./models');
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
+
 app.use(express.json());
 
-sequelize.authenticate()
-    .then(() => console.log('Database connected...'))
-    .catch(err => console.log('Error: ' + err));
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/products', require('./routes/productRoutes'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    await syncModels();
     console.log(`Server running on port ${PORT}`);
 });
